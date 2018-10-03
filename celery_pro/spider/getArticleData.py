3474af2bce6eee1268bbb412a3e52d5bc08d5207
myname <myemail@email.com>
2018-10-03 08:36:05 +0800

    [add] 新增爬取文章

--- celery_pro/spider/getArticleData.py
+++ celery_pro/spider/getArticleData.py
@@ -14,13 +14,12 @@ class GetArticleData(object):
         self.q = Queue()
 
         self.index_url = "http://www.908282.com/bbs/888.html"  # 第一网站基础url
-        self.xpath_pattern_1 = "/html/body/table/tbody/tr/td/a"  # 第一网站获取文章规则（a标签）
+        self.xpath_pattern_1 = "//tr/td/a"  # 获取文章规则（a标签）
+        self.xpath_pattern_2 = "//tr/td/p/font/b"  # 获取文章内容规则1
+        self.xpath_pattern_3 = "//tr/td/font"  # 获取文章标题2
 
-        self.xpath_pattern_2 = "//div[5]/div/h2/font/text()"  # 第一网站获取图片标题
         self.index_url_2 = "http://9542.34266.com/9542zl/tg.htm"  # 第二网站基础url
         self.last_url = "http://908181.com/caitu/%E7%BA%A2%E8%B4%A2%E7%A5%9E.gif"
-        self.xpath_pattern_3 = "//tr[2]/td/a/span/font/text()"  # 第一网站筛选期数规则
-
         self.xpath_pattern_4 = "//tr[1]/td[1]/font/span/a/font/text()"  # 第二网站的期数筛选规则
         self.get_periods_url_1 = "http://www.908282.com/908181.html"  # 第一网站筛选期数 url
         self.get_periods_url_2 = "http://9542.34266.com/9542zl/888.htm"  # 第二期数筛选期数url
@@ -140,99 +139,39 @@ class GetArticleData(object):
         return periods
 
     def run_article_1(self):
-        """  第一个网站  """
+        """  908282网站文章  """
+        # 第一层获取文章链接
+        response = self.get_response(url=self.index_url)
+        data_list = self.deal_data(html=response, xpath_pattern=self.xpath_pattern_1)
+        for data in data_list:
+            href = data.xpath("@href")[0]
+            title = data.xpath("text()")
+            if len(href[0]) <= 17:
+                href = "http://www.908282.com" + href[2:]
+            print(href, title, "=========>success")
 
-        periods = self.check_new_periods(source_type=1)
-        if not periods:
-            return False
-        article_response = self.get_response(url=self.index_url, response_type=3)
-        article_data_list = self.deal_data(html=article_response, xpath_pattern=self.xpath_pattern_1)
-        for article_data in article_data_list:
-            article_title = article_data.xpath("text()")
+            try:
+                article_response = self.get_response(url=href)
+            except:
+                print(href, title, "=========>fail")
 
-        for type_num in range(1, total_picture + 1):
-            # 第一个网站拼接url
-            if len(str(type_num)) == 1:
-                type_string = "0" + "0" + str(type_num)
-            elif len(str(type_num)) == 2:
-                type_string = "0" + str(type_num)
+                continue
+            if self.deal_data(html=article_response, xpath_pattern=self.xpath_pattern_2):
+                article_list = self.deal_data(html=article_response, xpath_pattern=self.xpath_pattern_2)
+            elif self.deal_data(html=article_response, xpath_pattern=self.xpath_pattern_3):
+                article_list = self.deal_data(html=article_response, xpath_pattern=self.xpath_pattern_3)
             else:
-                type_string = str(type_num)
-            url = self.index_url
-            url += type_string
-            url += ".html"
-            img_pattern = self.xpath_pattern_1
-            title_pattern = self.xpath_pattern_2
-            try:
-                html = self.get_response(url=url)
-                img_src = self.deal_data(html=html, xpath_pattern=img_pattern)[0]  # 图片链接
-                img_title = self.deal_data(html=html, xpath_pattern=title_pattern)[0]  # 图片标题
-            except:
-                self.q.put(url)
                 continue
-            if not all([img_src, img_title]):
-                print("==%s图片下载失败==url==%s" % (img_title, url))
-                continue
-            print("==============", type_num, img_src)
-            # 获取图片数据src
-            try:
-                if type_num == 15 or type_num == 16:
-                    img_src = "http://908181.com/" + img_src
-                img_content = self.get_response(url=img_src, response_type=2)
-            except:
-                print(type_num, img_title, "=======下载失败")
-                continue
-
-            # 检查数据库是否已经保存图片
-            is_save = self.check_is_save(periods=periods, source=self.index_url, img_src=img_src)
-            if is_save:
-                print("=================img has been save", periods, img_title, img_src)
-                continue
-
-            # 存入七牛，返回字典
-            data = self.save_in_qiniu(type_num=type_num, img_content=img_content, img_title=img_title,
-                                      img_src=img_src, periods=periods)
-            # 存入数据库
-            self.save_to_db(**data)
-            # # 下载失败后在队列中取出来，重新请求下载
-            # while not self.q.empty():
-            #     a = self.q.get()
-            #     print(a, "qqqqqqq")
-
-    def run_picture_2(self):
-        """  第二个网站  """
-        periods = self.check_new_periods(source_type=2)
-        if not periods:
-            return False
-        img_response = self.get_response(url=self.index_url_2)
-        img_url_list = self.deal_data(html=img_response, xpath_pattern=self.xpath_pattern_8)
-        type_num = 57
-        for img in img_url_list:
-            img_url = "http://9542.34266.com/9542zl/" + img.xpath("@href")[0]  # 图片url
-            img_title = img.xpath("text()")[0]  # 图片标题
-            img_res = self.get_response(url=img_url, response_type=3)  # 请求图片url返回体
-            img_src = self.deal_data(html=img_res, xpath_pattern=self.xpath_pattern_5)[0]  # 图片src
-            img_content = self.get_response(url=img_src, response_type=2)  # 请求src返回体，图片数据
-
-            # 检查图片是否已经存入数据库
-            is_save = self.check_is_save(periods=periods, source=self.index_url_2, img_src=img_src)
-            if is_save:
-                print("=================img has been save", periods, img_title, img_src)
-                continue
-            # 图片存入七牛云
-            data = self.save_in_qiniu(type_num=type_num, img_content=img_content, img_title=img_title, img_src=img_src,
-                                      periods=periods)
-            # 图片存入数据库
-            self.save_to_db(**data)
-            type_num += 1
+            for data in article_list:
+                text = data.xpath("text()")[0].strip()
+                if text:
+                    print(text)
+                else:
+                    continue
 
     def run(self):
-        if self.first in [1, "1"]:
-            self.run_picture_1()
-        if self.second in [1, "1"]:
-            self.run_picture_2()
-        return True
+        self.run_article_1()
 
-# # 测试！！！！！！！！！！
-# obj = GetPictureData()
-# obj.run()
+
+obj = GetArticleData()
+obj.run_article_1()
